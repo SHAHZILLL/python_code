cb059806b884fb5126c70248f5bc3ac727ac80b8
SHAHZILLL <shahzil.asif.9@gmail.com>
2025-02-10 16:33:45 +0500

    sec committ

--- The-Ultimate-Python-Course-main/Chapter 10/03_self.py
+++ The-Ultimate-Python-Course-main/Chapter 10/03_self.py
@@ -1,9 +1,9 @@
 class Employee: 
     language = "Python" # This is a class attribute
-    salary = 1200000
+    payment = 1200000
 
     def getInfo(self):
-        print(f"The language is {self.language}. The salary is {self.salary}")
+        print(f"The language is {self.language}. The payment is {self.payment}")
 
     @staticmethod
     def greet():
